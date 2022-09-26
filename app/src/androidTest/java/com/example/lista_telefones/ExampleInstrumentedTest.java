7f99e31cfdb18eb4eb3ef4ce441615687e317229
Jose <802.275@alunos.unigran.br>
2022-09-26 01:53:11 -0400

    inicio

--- app/src/androidTest/java/com/example/lista_telefones/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lista_telefones/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.lista_telefone;
+package com.example.lista_telefones;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.lista_telefone", appContext.getPackageName());
+        assertEquals("com.example.lista_telefones", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/lista_telefones/MainActivity.java
+++ app/src/main/java/com/example/lista_telefones/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.lista_telefone;
+package com.example.lista_telefones;
 
 import androidx.appcompat.app.AppCompatActivity;
 
